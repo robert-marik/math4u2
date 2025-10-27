10f525937efbd0f8890d209270392ff567d01267
Robert Marik <marik@mendelu.cz>
2025-10-27 10:26:52 +0100

    Remove utterances

--- scripts/math4u/__init__.py
+++ scripts/math4u/__init__.py
@@ -113,13 +113,6 @@ FOOT = """
 <img src="../foot.svg" style="width: 100% !important; max-width: 100% !important;">
 <div>
 
-<script src="https://utteranc.es/client.js"
-        repo="robert-marik/math4u-comments"
-        issue-term="title"
-        theme="github-light"
-        crossorigin="anonymous"
-        async>
-</script>
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
